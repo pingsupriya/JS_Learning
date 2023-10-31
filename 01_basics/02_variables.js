d9998fef52bcd50b80851bf2847caeab8edb8289
Supriya Wakchaure <96832103+pingsupriya@users.noreply.github.com>
2023-10-31 18:10:50 +0000

    basics 3 lectures learning

--- 01_basics/02_variables.js
+++ 01_basics/02_variables.js
@@ -3,7 +3,7 @@ let accountEmail = "supriya@google.com" //variable values can be changed
 var accountPassword = "12345" //prefer not to use var becoz of issue in block scope and functional scope
 accountCity = "Jaipur" //prefer not to use
 let accountState; //undefined
-// accountId = 2 //not allowed
+// accountId = 2 //not allowed as it is const
 accountEmail = "pr@pr.com"
 accountPassword = "21212"
 console.log(accountId);
---
+++ 01_basics/03_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";   //treat all JS code as newer version
+
+// alert("hello") //we are using nodeJS not browser 
+//refer mdn docs
+
+let name="supriya" //string(can also be in '')
+let age = 19 //number
+let isLogin = false//boolean
+
+//Primitive DataTypes
+
+// BigInt
+// null(standalone value)
+// undefined
+// symbol(unique)
+
+
+// Non_Primitve DataTypes
+
+//Object
+
+console.log(typeof"supriya") //output as String
+
+console.log(typeof null); //object (loophole in language as many error occurs of this)
+console.log(typeof undefined); //undefined
+
+
+
+
+
