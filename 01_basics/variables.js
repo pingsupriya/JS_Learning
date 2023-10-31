44b677e00819ec75c9b59f09bf7e348cb5f71c20
Supriya Wakchaure <96832103+pingsupriya@users.noreply.github.com>
2023-10-31 11:50:45 +0000

    container variables done

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 5415 //to assign a const value(locked value)
+let accountEmail = "supriya@google.com" //variable values can be changed
+var accountPassword = "12345" //prefer not to use var becoz of issue in block scope and functional scope
+accountCity = "Jaipur" //prefer not to use
+let accountState; //undefined
+// accountId = 2 //not allowed
+accountEmail = "pr@pr.com"
+accountPassword = "21212"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
