04f25b429e4b3a8054f8d7e5749126998c86e2f7
Supriya Wakchaure <96832103+pingsupriya@users.noreply.github.com>
2023-10-31 19:30:06 +0000

    operations in js

--- 01_basics/04_conversionOperations.js
+++ 01_basics/04_conversionOperations.js
@@ -1,11 +1,14 @@
+//**************************CONVERSION*************************************
+
 let score = "40ABC"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);//Number
-console.log(valueInNumber); //NaN(NOT A NUMBER)
+
+// console.log(typeof valueInNumber);//Number
+// console.log(valueInNumber); //NaN(NOT A NUMBER)
 
 //ouputs for ValueInNumber for different inputs
 //"40"=>40 
@@ -14,7 +17,8 @@ console.log(valueInNumber); //NaN(NOT A NUMBER)
 
 let a=1
 let booleanA = Boolean(a)
-console.log(booleanA);
+
+// console.log(booleanA);
 
 //1=>true 0=>false
 //""=>false
@@ -22,5 +26,30 @@ console.log(booleanA);
 
 let num = 33
 let StringNum = String(num)
-console.log(typeof StringNum);
-console.log(StringNum);
+
+// console.log(typeof StringNum);
+// console.log(StringNum);
+
+//**************************OPERATIONS***********************************
+
+let value=3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2**3);// (2^3)=8
+
+// console.log(2/3);//0.6666666666666666
+
+// console.log(2%3)//2
+
+let str1 = "hello"
+let str2 = " supriya"
+
+// console.log(str1+str2);
+// console.log(1+2+"3"); //33
+// console.log("1"+2+3); //123
+
+//do not use these type of syntax
+// console.log(+true);
+// console.log(+"");
