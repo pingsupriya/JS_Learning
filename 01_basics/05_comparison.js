97cd2a99f32fe4440af180769a576e1d89d6e23b
Supriya Wakchaure <96832103+pingsupriya@users.noreply.github.com>
2023-10-31 19:45:48 +0000

    Comparison operators

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1); //true (js auto converts 2 to num)
+
+//AVOID THESE NULL AND UNDEFINED VALUES
+
+// console.log(null==0); //false
+// console.log(null>=0); //true
+// console.log(null>0); //false
+
+// console.log(undefined==0); //false
+// console.log(undefined>=0); //false
+// console.log(undefined>0); //false
+
+//STRICT CHECK
+// (=== CHECKS VALUE AND DATATYPE TOO)
+
+console.log("2"===2); //false
