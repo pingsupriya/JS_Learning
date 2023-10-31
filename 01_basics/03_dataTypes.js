ce97d3156dd7280387bffe7cbedd41d3f90cb7d3
Supriya Wakchaure <96832103+pingsupriya@users.noreply.github.com>
2023-10-31 20:09:36 +0000

    Some more information of Datatypes

--- 01_basics/03_dataTypes.js
+++ 01_basics/03_dataTypes.js
@@ -6,23 +6,43 @@
 let name="supriya" //string(can also be in '')
 let age = 19 //number
 let isLogin = false//boolean
+const id1=symbol('12');
+const id2=symbol('12');
+// console.log(id1===id2); //false
+const bignum = 45621478521n
 
-//Primitive DataTypes
+console.log(typeof"supriya") //output as String
+console.log(typeof null); //object (loophole in language as many error occurs of this)
+console.log(typeof undefined); //undefined
 
+//Primitive DataTypes(7 types)--call by value
+
+//String
+//number
+//Boolean
 // BigInt
-// null(standalone value)
-// undefined
+// null(standalone value)--empty
+// undefined--var declared but value not given
 // symbol(unique)
 
 
-// Non_Primitve DataTypes
+// Non_Primitve DataTypes--call by ref
+//return type--function
+//Objects
+//Functions
+//array
 
-//Object
+const heros = ["shakti","mukti","doga"] //array
 
-console.log(typeof"supriya") //output as String
+let myObj={
+    name: "supriya",
+    age: 19
+}
 
-console.log(typeof null); //object (loophole in language as many error occurs of this)
-console.log(typeof undefined); //undefined
+let myFunction = function(){
+    console.log("hello");
+}
+
 
 
 
