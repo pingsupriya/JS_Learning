72b1b5482cd2b15e37491bdea221333afdc13f41
Supriya Wakchaure <96832103+pingsupriya@users.noreply.github.com>
2023-11-06 18:25:36 +0000

    Functions in js

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,65 @@
+function myname(){
+    console.log("supriya");
+}
+
+//reference myname
+//execution myname()
+
+// parameters--when function definition is written and the values passed are called parameters
+
+// arguments-when function calling is written and the values passed are called arguments 
+
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("supriya"))
+// console.log(loginUserMessage("supriya"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
